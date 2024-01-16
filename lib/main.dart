4d54a47f67efbb62fc22fd472bb2fc6c099400cd
Meshva Patel <136339359+Meshva30@users.noreply.github.com>
2024-01-16 17:26:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -434,3 +434,149 @@ class MyApp extends StatelessWidget {
         ));
   }
 }
+//1
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: true,
+//       home: SafeArea(
+//         child: Scaffold(
+//           appBar: AppBar(
+//             backgroundColor: Colors.blue,
+//             title: const Text(
+//               "Rich Text Example",
+//               style: TextStyle(
+//                   fontSize: 30,
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.white),
+//             ),
+//             centerTitle: true,
+//           ),
+//           body: Center(
+//             child: RichText(
+//               text: const TextSpan(children: [
+//                 TextSpan(
+//                     text: "Single\t",
+//                     style: TextStyle(
+//                         color: Colors.blue,
+//                         fontSize: 25,
+//                         fontWeight: FontWeight.bold)),
+//                 TextSpan(
+//                     text: " Line ",
+//                     style: TextStyle(
+//                         color: Colors.white,
+//                         fontSize: 25,
+//                         letterSpacing: 2,
+//                         backgroundColor: Colors.green,
+//                         fontWeight: FontWeight.bold)),
+//                 TextSpan(
+//                     text: " Multiple\t",
+//                     style: TextStyle(
+//                         color: Colors.orange,
+//                         fontSize: 25,
+//                         letterSpacing: 3,
+//                         fontWeight: FontWeight.bold)),
+//                 TextSpan(
+//                     text: " Styles",
+//                     style: TextStyle(
+//                         color: Colors.pink,
+//                         fontSize: 25,
+//                         fontStyle: FontStyle.italic,
+//                         fontWeight: FontWeight.bold)),
+//               ]),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//2.
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+          child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("Flutter Rich Text",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold)),
+        ),
+        body: Center(
+          child: RichText(
+            text: TextSpan(children: [
+              TextSpan(
+                  text: "RichText helps to create\n",
+                  style: TextStyle(color: Colors.grey, fontSize: 25)),
+              TextSpan(
+                text: "Highlighted\n",
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 45,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold),
+              ),
+              TextSpan(
+                text: "Clickable,",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 25,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+              TextSpan(
+                text: "OutlinedText.\n",
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 25,
+
+                ),
+
+              ),
+              TextSpan(
+                text: "Say Hi to RichText",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 25,
+
+                ),
+
+              ),
+
+
+
+            ]),
+          ),
+        ),
+      )),
+    );
+  }
+}
+
